c096758267024c93125ff7fc96eefa7043e73071
DecioLuiz <decioluiz@msn.com>
2023-07-07 16:21:28 -0300

    Operação da instanciação da base de dados

---
+++ src/main/java/com/decioluiz/springbootmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.decioluiz.springbootmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.decioluiz.springbootmongo.domain.User;
+import com.decioluiz.springbootmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
